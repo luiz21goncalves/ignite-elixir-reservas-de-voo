d7e607b49854d3a05542b51cb4a5d12cf3127e5f
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-30 15:45:17 -0300

    feat: generate report

---
+++ lib/bookings/report.ex
@@ -0,0 +1,27 @@
+defmodule Flightex.Bookings.Report do
+  alias Flightex.Bookings.Agent, as: BookingsAgent
+  alias Flightex.Bookings.Booking
+
+  def generate(filename \\ "report.csv") do
+    booking_list = build_booking_list()
+
+    File.write(filename, booking_list)
+  end
+
+  defp build_booking_list do
+    BookingsAgent.list_all()
+    |> Map.values()
+    |> Enum.map(fn booking -> booking_string(booking) end)
+  end
+
+  defp booking_string(%Booking{
+         user_id: user_id,
+         local_origin: local_origin,
+         local_destination: local_destination,
+         complete_date: complete_date
+       }) do
+    parsed_date = NaiveDateTime.to_string(complete_date)
+
+    "#{user_id},#{local_origin},#{local_destination},#{parsed_date}"
+  end
+end
--- lib/flightex.ex
+++ lib/flightex.ex
@@ -1,3 +1,12 @@
 defmodule Flightex do
+  alias Flightex.Bookings.CreateOrUpdate, as: BookingCreateOrUpdate
+  alias Flightex.Users.Agent, as: UserAgent
+  alias Flightex.Bookings.Agent, as: BookingAgent
 
+  def start_agents do
+    UserAgent.start_link(%{})
+    BookingAgent.start_link(%{})
+  end
+
+  defdelegate create_or_update_booking(params), to: BookingCreateOrUpdate, as: :call
 end
