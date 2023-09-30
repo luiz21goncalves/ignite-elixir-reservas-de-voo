56e1d29de5d58ce84608c491c2b41fd96b87c405
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-30 09:44:25 -0300

    feat: build user

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -3,7 +3,17 @@ defmodule Flightex.Users.User do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(name, email, cpf, id \\ UUID.uuid4()) do
+    if is_bitstring(cpf) do
+      {:ok,
+       %__MODULE__{
+         name: name,
+         email: email,
+         cpf: cpf,
+         id: id
+       }}
+    else
+      {:error, "Cpf must be a String"}
+    end
   end
 end
